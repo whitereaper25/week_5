2fdc1dc4141fc2bd2330edda9548509142066c36
arima <76896919+whitereaper25@users.noreply.github.com>
2022-01-23 19:19:26 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,48 @@
+import math   
+
+#I dont know the logic for division and modulus
+
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+        
+    def __add__(self, no):
+        real = self.real + no.real
+        imaginary = self.imaginary + no.imaginary
+        return Complex(real,imaginary)
+        
+    def __sub__(self, no):
+        real = self.real - no.real
+        imaginary = self.imaginary - no.imaginary
+        return Complex(real,imaginary)
+        
+    def __mul__(self, no):
+        real = self.real * no.real - self.imaginary * no.imaginary
+        imaginary = self.real * no.imaginary + self.imaginary * no.real
+        return Complex(real,imaginary)
+
+    def __truediv__(self, no):
+        
+    def mod(self):
+    
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
